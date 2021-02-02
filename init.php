560afa46f8490431664d56d41af4092f67a15021
fwmakc <fwmakc@mail.ru>
2021-02-02 19:44:38 +0300

    0.13

---
+++ init.php
@@ -0,0 +1,19 @@
+<?php
+
+// Рабочее пространство имен
+
+namespace is;
+
+// Базовые константы
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'defines.php';
+
+// определяем функции инициализации компонентов системы
+
+require_once __DIR__ . DS . 'init' . DS . 'config.php';
+
+// Include configuration file
+// Подключаем файл конфигурации
+//require_once PATH_SITE . 'configuration.php';
+
+?>
