247a9bd7c3933a1c9397c8f95425652663f86391
fwmakc <fwmakc@mail.ru>
2021-06-03 18:44:50 +0300

    1.0

--- Model/Masters/Extenders/Content/Content_all.php
+++ Model/Masters/Extenders/Content/Content_all.php
@@ -6,13 +6,13 @@ use is\Helpers\System;
 use is\Helpers\Strings;
 use is\Helpers\Objects;
 use is\Helpers\Prepare;
-use is\Parents\Entry;
+use is\Parents\Collection;
 use is\Masters\Database;
 use is\Components\Router;
 
 use is\Masters\Extenders\Process\Process;
 
-class Content extends Entry {
+class Content extends Collection {
 	
 	public function __construct() {
 		
@@ -26,18 +26,31 @@ class Content extends Entry {
 		$parents = System::typeIterable($parents) ? Strings::join($parents, ':+') : null;
 		
 		$db = Database::getInstance();
-		$db -> collection('content');
 		
+		$db -> collection('content');
 		if ($last) {
 			$db -> driver -> filter -> addFilter('name', '+' . $last);
-			$db -> driver -> filter -> addFilter('parents', '+' . $name . ($parents ? ':+' . $parents : null));
-			$db -> launch();
-			$this -> setEntry($db -> data -> getFirst());
 		}
+		$db -> driver -> filter -> addFilter('parents', '+' . $name . ($parents ? ':+' . $parents : null));
+		$db -> launch();
+		
+		if (!$db -> data -> getFirstData()) {
+			
+			$db -> clear();
+			
+			$db -> collection('content');
+			$db -> driver -> filter -> addFilter('parents', '+' . $name . ($parents ? ':+' . $parents : null) . ($last ? ':+' . $last : null));
+			$db -> launch();
+			
+		}
+		
+		$this -> addByList($db -> data -> getData());
 		
 		$db -> clear();
 		
 	}
+	
+	
 	
 }
 
