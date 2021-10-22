f9641f637495f39d502f981256c2c95be894c2b7
fwmakc <fwmakc@mail.ru>
2021-10-22 13:11:21 +0300

    Update autoload.php

--- controller/autoload.php
+++ controller/autoload.php
@@ -16,8 +16,13 @@ if (file_exists($autoload)) {
 } else {
 	
 	// manual loading
+	
+	// framework
+	
 	$autoload = $path . 'isengine' . DS . 'framework' . DS . 'php' . DS . 'init.php';
 	require_once $autoload;
+	
+	// classes
 	
 	$list = file_get_contents( DR . 'config' . DS . 'classes.ini' );
 	$list = json_decode($list, true);
@@ -32,7 +37,49 @@ if (file_exists($autoload)) {
 	
 	unset($list, $path);
 	
-}
+	// core model, app and modules
+	
+	$path = file_get_contents( DR . 'config' . DS . 'path.ini' );
+	$path = json_decode($path, true);
+	
+	$paths = [
+		'app' => $path['app'] ? DR . preg_replace('/[\:\/\\\\]+/ui', DS, $path['app']) . DS : null,
+		'core' => realpath(__DIR__ . DS . DP . 'model') . DS,
+		'vendors' => $path['vendors'] ? DR . preg_replace('/[\:\/\\\\]+/ui', DS, $path['vendors']) . DS : null
+	];
 
+	spl_autoload_register(function($class) use ($paths) {
+		
+		if (class_exists($class)) {
+			return;
+		}
+		
+		$array = explode('\\', $class);
+		array_shift($array);
+		
+		$module = mb_strpos($class, 'is\\Masters\\Modules\\', 0);
+		$module = $module || $module === 0 ? true : null;
+		
+		$path = implode(DS, $array);
+		$vendors = $module ? implode(DS, array_slice($array, 2)) : null;
+		
+		foreach ($paths as $key => $item) {
+			if ($module) {
+				$file = $item . ($key === 'vendors' && $vendors ? $vendors : $path) . DS . 'class' . DS . end($array) . '.php';
+				if (file_exists($file)) {
+					require_once $file;
+				}
+			} elseif ($key !== 'vendors') {
+				$file = $item . $path . '.php';
+				if (file_exists($file)) {
+					require_once $file;
+				}
+			}
+		}
+		unset($item);
+		
+	});
+
+}
 
 ?>
